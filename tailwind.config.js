2a8b279455234c9a7254a53b362ec19e6d3849da
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-04-10 09:55:29 -0400

    removing twcss line-clamp requirement as plugin in now included in tw 3.3

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,6 @@ module.exports = {
         require('@tailwindcss/forms'),
         require('@tailwindcss/typography'),
         require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/line-clamp'),
     ],
 
 }
